23da87d3219d1d3a29959397a54bfbb7160155ff
fdx02 <franciscodelgado2002@gmail.com>
2024-11-30 21:58:29 -0300

    asdas

--- Player.cs
+++ Player.cs
@@ -3,33 +3,41 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public  float speed = 300.0f;
+	public float speed = 300.0f;
 	AnimationPlayer animacion;
+	Node2D palo;
+
+	bool fCorrer, fIdle, fAtaque;
 
 	public override void _Ready()
 	{
-			animacion = this.GetNode<AnimationPlayer>("Animacion");
+	
+		animacion = this.GetNode<AnimationPlayer>("Animacion");
+		palo = this.GetNode<Node2D>("HandEquip");
+		animacion.Play("Idle");
 
 	}
 
 
 	public override void _PhysicsProcess(double delta)
 	{
+
 		Vector2 velocity = Velocity;
-		
+
 
 		//Movimiento
 		Vector2 direction = Input.GetVector("left", "right", "up", "down").Normalized();
+
 		if (direction != Vector2.Zero)
 		{
 			velocity = direction * speed;
+			palo.Visible = false;
+			fCorrer = true;
 		}
 		else
 		{
-			if (!animacion.IsPlaying()){
-				animacion.Play("Idle");
-			}
-			
+			fIdle = true;
+			palo.Visible = false;
 			velocity = Vector2.Zero;
 		}
 
@@ -38,18 +46,35 @@ public partial class Player : CharacterBody2D
 
 
 		//golpe
-		if (Input.IsActionJustPressed("golpe")){
-
-				animacion.Play("Swing");
+		if (Input.IsActionJustPressed("golpe"))
+		{
+			fAtaque = true;
+			palo.Visible = true;
 		}
+
 	}
 }
 
 
-		// public const float JumpVelocity = -400.0f;
+// public const float JumpVelocity = -400.0f;
 
-		// // Handle Jump.
-		// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		// {
-		// 	velocity.Y = JumpVelocity;
-		// }
+// // Handle Jump.
+// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+// {
+// 	velocity.Y = JumpVelocity;
+// }
+
+
+
+
+// if(fAtaque){
+// 	animacion.Play("Swing");
+// } else if (fCorrer && !animacion.IsPlaying()){
+// 	animacion.Play("Correr");
+// } else if (fIdle && !animacion.IsPlaying()){
+// 	animacion.Play("Idle");
+// }
+// fAtaque = false;
+// fCorrer = false;
+// fIdle = false;
+
